ca083f79ea8e4b363d58fbbf8abe2901fb3c38c5
Charles <155798472+202210532@users.noreply.github.com>
2024-03-01 17:46:10 +0800

    Delete JS directory

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const body = document.body;
-  body.style.transition = 'opacity 1.062s';
-  body.style.opacity = 0;
-
-  window.onload = () => {
-    body.style.opacity = 1;
-  };
-});
